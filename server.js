2bbb2079bf97e70f82c8419b3016977dba23d2e0
Prasanth <prasanth924@live.com>
2016-04-06 23:18:17 -0500

    Updated the server

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+    id:1,
+    description:"Meet mom for lunch",
+    completed:false
+},
+{
+    id:2,
+    description:"Go to market",
+    completed:false
+},
+{
+    id:3,
+    description:"Feed my cat",
+    completed:false
+}
+];
 
 app.get('/',function(req,res){
 
     res.send('Todo API Root');
+
+});
+
+//GET request
+
+app.get('/todos',function(req,res){
+
+    res.json(todos);
+
+});
+
+//GET request by ID
+
+app.get('/todos',function(req,res){
 
 });
 
